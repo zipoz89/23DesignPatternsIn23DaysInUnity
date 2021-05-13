d0762fa45351b889a8214b0954f5869529d0735d
zipoz89 <31932780+zipoz89@users.noreply.github.com>
2021-05-14 00:23:50 +0200

    Add files via upload

---
+++ Assets/Scripts/Patterns/Observer/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Observer
+{
+    [SerializeField] private bool isActive = false;
+    Animator animator = null;
+    [SerializeField] private TriggerBox trigger = null;
+
+    private void Start() {
+        animator = this.GetComponent<Animator>();
+        trigger.RegisterObserver(this);
+    }
+
+    public override void OnNotify(object value, NotificationType type) {
+        if(type == NotificationType.TriggerDoorlikeObject)
+        if (isActive) {
+            if ((string)value == "open") {
+
+                if (!animator.GetBool("isDoorOpen")) {
+                    animator.SetBool("isDoorOpen", true);
+                }
+            }
+            else if ((string)value == "close") {
+
+                if (animator.GetBool("isDoorOpen")) {
+                    animator.SetBool("isDoorOpen", false);
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Patterns/Observer/Observer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Observer : MonoBehaviour
+{
+    public abstract void OnNotify(object value,NotificationType type);
+}
+
+public abstract class Subject : MonoBehaviour {
+    private List<Observer> observers = new List<Observer>();
+
+    public void RegisterObserver(Observer observer){
+        observers.Add(observer);
+    }
+    public void Notify(object value, NotificationType type) {
+        foreach (Observer o in observers) {
+            o.OnNotify(value,type);
+
+        }
+    }
+
+}
+
+
+public enum NotificationType {
+    TriggerDoorlikeObject
+}
