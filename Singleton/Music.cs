4cf7d10e8117a62dde8f6155ee49d6d6c42326cf
zipoz89 <thezipoz89@gmail.com>
2021-05-15 00:00:39 +0200

    Singleton

---
+++ Singleton/Music.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioListener))]
+[RequireComponent(typeof(AudioSource))]
+class Music : MonoBehaviour {
+    private static Music instance = null;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip[] clips;
+    public int musicToPlay = 0;
+
+    public static Music Instance {
+        get {
+            if (instance == null) {
+                instance = (Music)FindObjectOfType(typeof(Music));
+            }
+            return instance;
+        }
+    }
+
+    void Start() {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        if (audioSource.isPlaying == false) {
+            audioSource.clip = clips[musicToPlay];
+            audioSource.Play();
+        }
+    }
+
+    void Awake() {
+        if (Instance != this) {
+            Destroy(gameObject);
+        }
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
