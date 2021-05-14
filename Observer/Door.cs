4a4cb9148db5166bca3373b2977c0bfa382d00d8
zipoz89 <thezipoz89@gmail.com>
2021-05-15 01:22:53 +0200

    Refactor

---
+++ Observer/Door.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Observer
+{
+    [SerializeField] private bool isActive = false;
+    Animator animator = null;
+    [SerializeField] private TriggerBox trigger = null;
+
+    private void Start() {
+        animator = this.GetComponent<Animator>();
+        trigger.RegisterObserver(this);
+    }
+
+    public override void OnNotify(object value, NotificationType type) {
+        if(type == NotificationType.TriggerDoorlikeObject)
+        if (isActive) {
+            if ((string)value == "open") {
+
+                if (!animator.GetBool("isDoorOpen")) {
+                    animator.SetTrigger("triggerDoorKnob");
+                    animator.SetBool("isDoorOpen", true);
+                }
+            }
+            else if ((string)value == "close") {
+
+                if (animator.GetBool("isDoorOpen")) {
+                    animator.SetBool("isDoorOpen", false);
+                }
+
+            }
+        }
+    }
+}
---
+++ Observer/TriggerBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBox : Subject
+{
+    private void OnTriggerEnter(Collider other) {
+        Notify("open",NotificationType.TriggerDoorlikeObject);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Notify("close", NotificationType.TriggerDoorlikeObject);
+    }
+
+}
