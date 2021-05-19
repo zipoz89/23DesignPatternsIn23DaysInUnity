134952063033d94c4397ddbe85ca0cf1b08e6172
zipoz89 <thezipoz89@gmail.com>
2021-05-19 17:40:29 +0200

    Composite

---
+++ BaseScaneForFuture/PlayerMovement2d.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2d : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    [Range(0, .3f)] [SerializeField] private float smoothnigValue = .05f;
+    public bool isRunning = false;
+    public float moveSpeed = 10;
+
+    private Vector3 idkWhatItDoesButItMustBeZeroToWork = Vector3.zero;
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (isRunning)
+            animator.SetBool("isRunning", true);
+        else animator.SetBool("isRunning", false);
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        if (horizontalAxis != 0) {
+            isRunning = true;
+            if (horizontalAxis < 0)
+                this.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            else this.transform.localRotation = Quaternion.Euler(0,0, 0);
+            MoveCharacter(horizontalAxis);
+        }
+        else isRunning = false;
+    }
+
+    private void MoveCharacter(float horizontalAxis) {
+        Vector3 targetVeolicty = new Vector3(this.transform.position.x+ moveSpeed * horizontalAxis * Time.deltaTime, this.transform.position.y);
+        this.transform.position = targetVeolicty;
+    }
+}
---
+++ Composite/Atribute.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atribute
+{
+    protected float atributeBaseValue;
+    protected float multiplier;
+
+
+    public Atribute(float atributeValue, float multiplier = 0) {
+        this.atributeBaseValue = atributeValue;
+        this.multiplier = multiplier;
+    }
+
+    public float BaseValue() { return atributeBaseValue; }
+    public void SetValue(float value) { atributeBaseValue = value; }
+    public float BaseMultipier() { return multiplier; }
+
+    public void SetMultipier(float value) { multiplier = value; }
+}
+
+public class RawBonus : Atribute {
+    public RawBonus(float atributeValue, float multiplier) : base(atributeValue, multiplier) { }
+
+}
+
+public class FinalBonus : Atribute {
+    public FinalBonus(float atributeValue, float multiplier) : base(atributeValue, multiplier) { }
+
+}
+
+public class FinalAtribute : Atribute {
+    List<RawBonus> rawBonuses = new List<RawBonus>();
+    List<FinalBonus> finalBonuses = new List<FinalBonus>();
+
+    private float finalValue;
+
+    public FinalAtribute(float value) : base(value) {
+        finalValue = value;
+
+    }
+
+    public void addRawBonus(RawBonus bonus) {
+        rawBonuses.Add(bonus);
+    }
+
+    public void addFianlBonus(FinalBonus bonus) {
+        finalBonuses.Add(bonus);
+    }
+
+    public float calculateValue() {
+        finalValue = BaseValue();
+        float rawBonusValue = 0;
+        float rawBonusMultiplier = 0;
+
+        foreach (RawBonus r in rawBonuses) {
+            rawBonusValue += r.BaseValue();
+            rawBonusMultiplier += r.BaseMultipier();
+        }
+
+        finalValue += rawBonusValue;
+        finalValue *= (1 + rawBonusMultiplier);
+
+        float finalBonusValue =0;
+        float finalBonusMultiplier = 0;
+
+        foreach (FinalBonus f in finalBonuses) {
+            finalBonusValue += f.BaseValue();
+            finalBonusMultiplier += f.BaseMultipier();
+        }
+
+        finalValue += finalBonusValue;
+        finalValue *= (1 + finalBonusMultiplier);
+
+        return finalValue;
+    }
+}
