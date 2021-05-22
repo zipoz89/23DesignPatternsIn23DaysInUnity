3fd10d247e273208f24c200878df004d9a16d108
zipoz89 <thezipoz89@gmail.com>
2021-05-22 22:25:08 +0200

    proxy

---
+++ Proxy/LoginProxy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct LoginCredentials {
+    public string login;
+    public string password;
+}
+
+public interface ILogin {
+    int TryToLogin(string login,string password);
+    void updateDatabase(List<LoginCredentials> credentials);
+}
+
+[System.Serializable]
+public class Login : ILogin {
+
+    [SerializeField] private Dictionary<string, string> superSecureDatbase = new Dictionary<string, string>();
+    public int TryToLogin(string login, string password) {
+        string pulledPassword;
+        if (superSecureDatbase.TryGetValue(login, out pulledPassword)) {
+            if (pulledPassword == password) return 0;
+            else return 1;
+        }
+        else return 1;
+    }
+
+    public void updateDatabase(List<LoginCredentials> credentials) {
+        foreach (LoginCredentials c in credentials) {
+            superSecureDatbase.Add(c.login,c.password);
+        }
+    }
+}
+
+[System.Serializable]
+public class LoginProxy :  ILogin {
+    private Login loginSystem;
+
+
+    public LoginProxy(Login loginSystem) {
+        this.loginSystem = loginSystem;
+    }
+
+    public int TryToLogin(string login, string password) {
+        if (login.Length < 1 || password.Length < 1) {
+            return 1;
+        }
+        if (loginSystem.TryToLogin(login, password)==0) {
+            return 0;
+        }
+        else {
+            return 2;
+        }
+
+
+    }
+
+    public void updateDatabase(List<LoginCredentials> credentials) {
+        loginSystem.updateDatabase(credentials);
+    }
+}
---
+++ Proxy/LoginSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LoginSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField loginField;
+    [SerializeField] private TMP_InputField passwordField;
+    [SerializeField] private TextMeshProUGUI errorMessage;
+    Login login;
+    LoginProxy loginProxy;
+    [SerializeField] private List<LoginCredentials> credentialsToUpdate;
+    void Start()
+    {
+        login = new Login();
+        loginProxy = new LoginProxy(login);
+        loginProxy.updateDatabase(credentialsToUpdate);
+    }
+
+    public void LoginPressed() {
+        switch (loginProxy.TryToLogin(loginField.text, passwordField.text)) {
+            case 0:
+                SceneManager.LoadScene("SampleScene 1", LoadSceneMode.Single);
+                break;
+            case 1:
+                errorMessage.text = "Some fields are empty!";
+                break;
+            case 2:
+                errorMessage.text = "Login unsuccessful, check your credentials.";
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+}
