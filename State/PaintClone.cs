4ca8216c52915ea0888e62863be4c890c2a6e4d3
zipoz89 <thezipoz89@gmail.com>
2021-05-31 01:05:51 +0200

    State

---
+++ State/PaintClone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PaintClone : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private TileBase tile;
+    DrawingState[] states;
+    int currentState = 0;
+    DrawingMode mode = DrawingMode.Draw;
+    [SerializeField] private int radius = 1;
+    void Start()
+    {
+        states = new DrawingState[2];
+        states[0] = new CircleDrawingState();
+        states[1] = new SquareDrawingState();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (currentState == 0) currentState = 1;
+            else currentState = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)|| Input.GetKeyDown(KeyCode.LeftArrow)) {
+            if (mode == DrawingMode.Draw) mode = DrawingMode.Erase;
+            else mode = DrawingMode.Draw;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            radius++;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            radius--;
+            if (radius < 0) radius = 0;
+        }
+        if (Input.GetMouseButton(0)) {
+            Vector3Int tileToSet = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            states[currentState].SetCells(tileToSet,tilemap,tile, mode, radius);
+        }
+    }
+}
---
+++ State/State.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum DrawingMode {
+    Draw,
+    Erase
+}
+
+public interface DrawingState
+{
+    void SetCells(Vector3Int tileToSet, Tilemap tilemap, TileBase tile, DrawingMode mode, int radius);
+}
+
+public class CircleDrawingState : DrawingState {
+    public void SetCells(Vector3Int tileToSet, Tilemap tilemap, TileBase tile, DrawingMode mode,int radius) {
+        if (mode == DrawingMode.Erase) tile = null;
+        if (radius != 0)
+            for (int x = tileToSet.x - radius; x <= tileToSet.x + radius; x++) {
+                for (int y = tileToSet.y - radius; y <= tileToSet.y + radius; y++) {
+                    if ((x - tileToSet.x) * (x - tileToSet.x) + (y - tileToSet.y) * (y - tileToSet.y) <= radius * radius) {
+                        Vector3Int finalPos = new Vector3Int(x, y, 0);
+
+                        tilemap.SetTile(finalPos, tile);
+                        tilemap.RefreshTile(finalPos);
+                    }
+                }
+            }
+        else {
+            tilemap.SetTile(tileToSet, tile);
+            tilemap.RefreshTile(tileToSet);
+        }
+    }
+}
+public class SquareDrawingState : DrawingState {
+    public void SetCells(Vector3Int tileToSet, Tilemap tilemap, TileBase tile, DrawingMode mode, int radius) {
+        if (mode == DrawingMode.Erase) tile = null;
+        if (radius != 0)
+            for (int x = tileToSet.x - radius; x < tileToSet.x + radius; x++) {
+                for (int y = tileToSet.y - radius; y < tileToSet.y + radius; y++) {
+                    Vector3Int finalPos = new Vector3Int(x, y, 0);
+                    tilemap.SetTile(finalPos, tile);
+                    tilemap.RefreshTile(finalPos);
+                }
+            }
+        else {
+            tilemap.SetTile(tileToSet, tile);
+            tilemap.RefreshTile(tileToSet);
+        }
+    }
+}
